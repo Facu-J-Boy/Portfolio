bda722af8066e37440da1feb2c1556b7f12a56ca
Facu-J-Boy <boyfacundo45@gmail.com>
2023-03-21 19:45:01 -0300

    proyect responsive

--- src/Components/Proyects/AllProyects.js
+++ src/Components/Proyects/AllProyects.js
@@ -6,7 +6,39 @@ const allProyects = [
     {
         name: "proyecto2",
         image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544026/samples/landscapes/beach-boat.jpg"
-    }
+    },
+    {
+        name: "proyecto1",
+        image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544029/samples/landscapes/nature-mountains.jpg"
+    },
+    // {
+    //     name: "proyecto2",
+    //     image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544026/samples/landscapes/beach-boat.jpg"
+    // },
+    // {
+    //     name: "proyecto1",
+    //     image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544029/samples/landscapes/nature-mountains.jpg"
+    // },
+    // {
+    //     name: "proyecto2",
+    //     image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544026/samples/landscapes/beach-boat.jpg"
+    // },
+    // {
+    //     name: "proyecto1",
+    //     image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544029/samples/landscapes/nature-mountains.jpg"
+    // },
+    // {
+    //     name: "proyecto2",
+    //     image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544026/samples/landscapes/beach-boat.jpg"
+    // },
+    // {
+    //     name: "proyecto1",
+    //     image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544029/samples/landscapes/nature-mountains.jpg"
+    // },
+    // {
+    //     name: "proyecto2",
+    //     image: "https://res.cloudinary.com/dvqh0exq6/image/upload/v1675544026/samples/landscapes/beach-boat.jpg"
+    // }
 ]
 
 export default allProyects
