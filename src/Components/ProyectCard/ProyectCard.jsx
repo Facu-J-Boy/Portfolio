f704a455c7ff8e433d34808cb501c178c10cceed
Facu-J-Boy <boyfacundo45@gmail.com>
2023-03-24 18:34:33 -0300

    horizontal scroll image in proyect card

--- src/Components/ProyectCard/ProyectCard.jsx
+++ src/Components/ProyectCard/ProyectCard.jsx
@@ -5,7 +5,6 @@ const ProyectCard = ({ name, images }) => {
   return (
     <div className="proyect-card">
       <h1>{name}</h1>
-      {/* <img src={`${image}`} alt="imagen" /> */}
       <div className="slider-container">
         {images.map((el, index) => (
           <img key={index} className="slider-item" src={`${el}`} alt="imagen" />
