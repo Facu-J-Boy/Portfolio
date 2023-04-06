cdbee61a73eae934d3eed17114699d5adaa42e84
Facu-J-Boy <boyfacundo45@gmail.com>
2023-04-06 20:03:27 -0300

    cv download

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -35,22 +35,24 @@ const Contact = () => {
             </svg>
           </button>
         </a>
-        <button className="icon-btn">
-          <svg viewBox="0 0 100 100">
-            <circle cx="50" cy="50" r="50" fill="#007bff" />
-            <text
-              x="50"
-              y="60"
-              fill="#ffffff"
-              fontSize="50"
-              fontWeight="bold"
-              textAnchor="middle"
-              dominantBaseline="middle"
-            >
-              CV
-            </text>
-          </svg>
-        </button>
+        <a href="./FacundoBoy.pdf" download>
+          <button className="icon-btn">
+            <svg viewBox="0 0 100 100">
+              <circle cx="50" cy="50" r="50" fill="#007bff" />
+              <text
+                x="50"
+                y="60"
+                fill="#ffffff"
+                fontSize="50"
+                fontWeight="bold"
+                textAnchor="middle"
+                dominantBaseline="middle"
+              >
+                CV
+              </text>
+            </svg>
+          </button>
+        </a>
       </div>
       <div className="contacts-btn">
         <a href={`${process.env.REACT_APP_LINKEDIN_URL}`}>
@@ -85,45 +87,47 @@ const Contact = () => {
             </svg>
           </button>
         </a>
-        <button className="download-button">
-          <div className="docs">
-            <svg
-              className="css-i6dzq1"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              fill="none"
-              strokeWidth="2"
-              stroke="currentColor"
-              height="20"
-              width="20"
-              viewBox="0 0 24 24"
-            >
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-              <polyline points="14 2 14 8 20 8"></polyline>
-              <line y2="13" x2="8" y1="13" x1="16"></line>
-              <line y2="17" x2="8" y1="17" x1="16"></line>
-              <polyline points="10 9 9 9 8 9"></polyline>
-            </svg>{" "}
-            CV
-          </div>
-          <div className="download">
-            <svg
-              className="css-i6dzq1"
-              strokeLinejoin="round"
-              strokeLinecap="round"
-              fill="none"
-              strokeWidth="2"
-              stroke="currentColor"
-              height="24"
-              width="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-              <polyline points="7 10 12 15 17 10"></polyline>
-              <line y2="3" x2="12" y1="15" x1="12"></line>
-            </svg>
-          </div>
-        </button>
+        <a href="./FacundoBoy.pdf" download>
+          <button className="download-button">
+            <div className="docs">
+              <svg
+                className="css-i6dzq1"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                fill="none"
+                strokeWidth="2"
+                stroke="currentColor"
+                height="20"
+                width="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                <polyline points="14 2 14 8 20 8"></polyline>
+                <line y2="13" x2="8" y1="13" x1="16"></line>
+                <line y2="17" x2="8" y1="17" x1="16"></line>
+                <polyline points="10 9 9 9 8 9"></polyline>
+              </svg>{" "}
+              CV
+            </div>
+            <div className="download" href="./cv.pdf" download>
+              <svg
+                className="css-i6dzq1"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                fill="none"
+                strokeWidth="2"
+                stroke="currentColor"
+                height="24"
+                width="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                <polyline points="7 10 12 15 17 10"></polyline>
+                <line y2="3" x2="12" y1="15" x1="12"></line>
+              </svg>
+            </div>
+          </button>
+        </a>
       </div>
     </div>
   );
