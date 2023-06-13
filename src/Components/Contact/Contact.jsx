834207310e30ea2d5ea60e40c6c254058fc0edf3
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-12 21:02:33 -0300

    cv icon download change

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
             </svg>
           </button>
         </a>
-        <a href="./FacundoBoy.pdf" download>
+        <a href="./Facundo Boy.pdf" download>
           <button className="icon-btn">
             <svg viewBox="0 0 100 100">
               <circle cx="50" cy="50" r="50" fill="#007bff" />
