b7698d3ec8fdeaf59a27de2a3d6e94d101c69336
Facu-J-Boy <boyfacundo45@gmail.com>
2024-01-17 17:53:34 -0300

    E-commerce

--- src/Components/Proyects/AllProyects.js
+++ src/Components/Proyects/AllProyects.js
@@ -1,35 +1,49 @@
 const allProyects = [
-    {
-        name: "PI-Videogame",
-        images: [
-            "/proyects/PI-Videogame/PI-Videogame1.png",
-            "/proyects/PI-Videogame/PI-Videogame2.png",
-            "/proyects/PI-Videogame/PI-Videogame3.png"
-        ],
-        url: "https://pi-videogame-lyart.vercel.app"
-    },
-    {
-        name: "Henry-Instrument",
-        images: [
-            "/proyects/Henry-Instrument/Henry-instrument1.png", 
-            "/proyects/Henry-Instrument/Henry-instrument2.png",
-            "/proyects/Henry-Instrument/Henry-instrument3.png",
-            "/proyects/Henry-Instrument/Henry-instrument4.png",
-            "/proyects/Henry-Instrument/Henry-instrument5.png"
-        ],
-        url: "https://henry-instruments-nu.vercel.app"
-    },
-    {
-        name: "ToDo",
-        images: [
-            "/proyects/ToDo/ToDo1.png",
-            "/proyects/ToDo/ToDo2.png",
-            "/proyects/ToDo/ToDo3.png",
-            "/proyects/ToDo/ToDo4.png",
-            "/proyects/ToDo/ToDo5.png"
-        ],
-        url: "https://todolist332.netlify.app"
-    }
-]
+  {
+    name: 'PI-Videogame',
+    images: [
+      '/proyects/PI-Videogame/PI-Videogame1.png',
+      '/proyects/PI-Videogame/PI-Videogame2.png',
+      '/proyects/PI-Videogame/PI-Videogame3.png',
+    ],
+    url: 'https://pi-videogame-lyart.vercel.app',
+  },
+  {
+    name: 'Henry-Instrument',
+    images: [
+      '/proyects/Henry-Instrument/Henry-instrument1.png',
+      '/proyects/Henry-Instrument/Henry-instrument2.png',
+      '/proyects/Henry-Instrument/Henry-instrument3.png',
+      '/proyects/Henry-Instrument/Henry-instrument4.png',
+      '/proyects/Henry-Instrument/Henry-instrument5.png',
+    ],
+    url: 'https://henry-instruments-nu.vercel.app',
+  },
+  {
+    name: 'ToDo',
+    images: [
+      '/proyects/ToDo/ToDo1.png',
+      '/proyects/ToDo/ToDo2.png',
+      '/proyects/ToDo/ToDo3.png',
+      '/proyects/ToDo/ToDo4.png',
+      '/proyects/ToDo/ToDo5.png',
+    ],
+    url: 'https://todolist332.netlify.app',
+  },
+  {
+    name: 'E-commerce',
+    images: [
+      '/proyects/E-commerce/E-commerce1.png',
+      '/proyects/E-commerce/E-commerce2.png',
+      '/proyects/E-commerce/E-commerce3.png',
+      '/proyects/E-commerce/E-commerce4.png',
+      '/proyects/E-commerce/E-commerce5.png',
+      '/proyects/E-commerce/E-commerce6.png',
+      '/proyects/E-commerce/E-commerce7.png',
+      '/proyects/E-commerce/E-commerce8.png',
+    ],
+    url: 'https://e-commerce-sigma-navy.vercel.app',
+  },
+];
 
-export default allProyects
+export default allProyects;
