313b3549e39365d9ab734cdb3ef99908cd519b33
Facu-J-Boy <boyfacundo45@gmail.com>
2024-01-19 19:34:05 -0300

    fix: cambios en aboutMe

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
-import './AboutMe.css';
+import styles from './AboutMe.module.css';
 import info from './info';
 import profile from './profile/profile.jpg';
 
 const AboutMe = () => {
   return (
-    <>
-      <h1 id="aboutMe-title">SOBRE MI</h1>
-      <div className="component-aboutMe">
-        <h1>¿Quien soy?</h1>
-        {/* <p>
-          Mi nombre es Facundo Boy, soy de Argentina y soy fullstack
-          developer, graduado del Bootcamp de Henry, tengo experiencia
-          con Javascript, React.js, Redux, HTML, CSS, express,
-          sequelize y postgreSQL. Estoy en busca de mi primer trabajo
-          como programador con ganas de aportar en nuevos proyectos,
-          aprender más, mejorar mis habilidades y conocer gente,
-          tambien cuento con experiencia trabajando en equipo con
-          metodología scrum.
-        </p>
-        <h1>¿Qué hago?</h1>
+    <div div className={styles.aboutMe}>
+      {/* <h1 id="aboutMe-title">SOBRE MI</h1> */}
+      {/* <div className="component-aboutMe"> */}
+      {/* <h1>¿Quien soy?</h1> */}
+      <div className={styles.info}>
+        <h1>{info.name}</h1>
         <p>
-          Actualmente realizo proyectos para seguir mejorando mis
-          habilidades como programador aprendiendo nuevas tecnologías.
-        </p> */}
-        <p>{info.info}</p>
-        <div>
-          <img src={profile} alt="Facundo Boy" />
-        </div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Mollitia quas pariatur nihil, amet provident quibusdam
+          dolores autem, accusamus reprehenderit, doloremque
+          assumenda! Iure obcaecati odio illo. Repellat labore facere
+          non. Iusto?
+        </p>
       </div>
-    </>
+      {/* <p>{info.info}</p> */}
+      <div className={styles.imageContainer}>
+        <img src={profile} alt="Facundo Boy" />
+      </div>
+      {/* </div> */}
+    </div>
   );
 };
 
