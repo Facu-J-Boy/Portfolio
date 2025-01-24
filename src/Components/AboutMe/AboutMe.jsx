38e40315719eb61454f2914435386a475e7fe3f2
Facu-J-Boy <boyfacundo45@gmail.com>
2025-01-24 20:25:45 -0300

    info settings

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './AboutMe.module.css';
-import info from './info';
 import profile from './profile/profile.jpg';
 
 const AboutMe = () => {
@@ -11,17 +10,17 @@ const AboutMe = () => {
           <img src={profile} alt="Facundo Boy" />
         </div>
         <div className={styles.title}>
-          <h1>{info.name}</h1>
+          <h1>Facundo Boy</h1>
           <h3>Desarrollador web</h3>
         </div>
       </div>
       <p>
         Desarrollador enfocado en React y TypeScript para la creación
         de interfaces de usuario dinámicas. Mi experiencia se extiende
-        al desarrollo de API's CRUD utilizando Node.js, con bases de datos SQL y NoSQL
-        para el backend. Apasionado por construir soluciones
-        eficientes y modernas, estoy listo para aportar a proyectos
-        que busquen innovación y calidad técnica.
+        al desarrollo de API's CRUD utilizando Node.js, con bases de
+        datos SQL y NoSQL para el backend. Apasionado por construir
+        soluciones eficientes y modernas, estoy listo para aportar a
+        proyectos que busquen innovación y calidad técnica.
       </p>
     </div>
   );
